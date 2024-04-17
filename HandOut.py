a74b271c0ca5492af40aef6c85b5555fdb6df6f4
Keven <cotek99@hotmail.com>
2024-04-16 21:08:19 -0400

    16 avril 2024

---
+++ HandOut.py
@@ -0,0 +1,13 @@
+'''
+
+    Info Git:
+
+    git pull = downloader la dernière version
+    git status = Voir l'état des dossiers.
+    git add "nom du fichier"
+    git commit
+    git push
+
+
+
+'''
