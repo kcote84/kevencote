02f9f303585148fc381296ede1eb1edcb8f262e0
Keven <cotek99@hotmail.com>
2024-11-16 17:53:02 -0500

    16 NOV 1752

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 
 '''
     Info Git:
+    git init = la toute première fois pour créer le fichier cacher dans le répertoir pour le suivit git.
+
     git pull = downloader la dernière version
     git status = Voir l'état des dossiers.
     git add "nom du fichier"
-    git commit
+    git commit -m "nom du commit"
     git push
+
+
+    git log = voir les derniers commit ( les ID)
+    git checkout <id du commit> = permet de revenir à une ancienne version.
 '''
